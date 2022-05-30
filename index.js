35b43e30113fc5398f9658eac0eef3b694ec78f2
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-05-30 19:23:56 -0400

    Set host to 0

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ fastify.route({
 
 const start = async () => {
   try {
-    await fastify.listen(PORT); //, "0.0.0.0");
+    await fastify.listen(PORT, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
