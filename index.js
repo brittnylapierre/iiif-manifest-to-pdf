1924b55cad4de7e3c1679486d23b81953a08144e
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-06-01 10:39:08 -0400

    Heroku host

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ fastify.route({
 
 const start = async () => {
   try {
-    await fastify.listen(PORT);
+    await fastify.listen(PORT, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
