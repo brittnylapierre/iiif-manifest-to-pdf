c9e54d8e40695f685e314b66d3bc525a4e1b7b19
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-06-01 10:37:24 -0400

    Update websocket module

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import * as Fastify from "fastify";
 import * as FastifyStatic from "@fastify/static";
+import * as FastifyWS from "@fastify/websocket";
 import createHash from "hash-generator";
 import Cocktail from "./cocktail.js";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import * as FastifyWS from "fastify-websocket";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
