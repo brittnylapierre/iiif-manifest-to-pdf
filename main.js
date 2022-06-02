3a93c0d36c359d77820298244728533762155cf5
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-06-02 17:44:39 -0400

    better aspect ratio

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const createHash = require("hash-generator");
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       //preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
